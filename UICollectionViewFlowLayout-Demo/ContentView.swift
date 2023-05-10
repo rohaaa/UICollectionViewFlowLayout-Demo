8ce4da522ba8c0336c9feecaac2782597a179cd0
Kaguermanov Armand <armand.kaguermanov@gmail.com>
2023-05-10 14:32:58 +0200

    Initial Commit

---
+++ UICollectionViewFlowLayout-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UICollectionViewFlowLayout-Demo
+//
+//  Created by Armand Kaguermanov on 10/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UICollectionViewFlowLayout-Demo/UICollectionViewFlowLayout_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICollectionViewFlowLayout_DemoApp.swift
+//  UICollectionViewFlowLayout-Demo
+//
+//  Created by Armand Kaguermanov on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UICollectionViewFlowLayout_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
